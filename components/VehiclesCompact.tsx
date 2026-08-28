095cbc59e40602d52ac05ef86e8f76db2c3d4117
herramientasseo4-sketch <herramientasseo4@gmail.com>
2026-08-28 10:45:16 -0600

    Update VehiclesCompact.tsx

--- components/VehiclesCompact.tsx
+++ components/VehiclesCompact.tsx
@@ -110,7 +110,7 @@ export default function VehiclesCompact({ vehicles, priceLine }: Props) {
                   {description}
                 </p>
 
-            {(name.toLowerCase().includes('rzr') || priceLine?.[vehicle._id]) && (
+            {priceLine?.[vehicle._id] && (
                   <div style={{
                     marginTop: '0.5rem',
                     fontSize: '0.85rem',
@@ -118,9 +118,12 @@ export default function VehiclesCompact({ vehicles, priceLine }: Props) {
                     borderTop: '1px solid rgba(255,255,255,0.1)',
                     paddingTop: '0.5rem',
                   }}>
-                    {name.toLowerCase().includes('rzr') ? '$2900 MXN' : priceLine?.[vehicle._id]}
+                    {priceLine[vehicle._id]}
                   </div>
-                )
+                )}
+              </div>
+            </div>
+          )
         })}
       </div>
 
