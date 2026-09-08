d8c3ae39f2c5c12a797a5ead6ed64a61bf928f83
herramientasseo4-sketch <herramientasseo4@gmail.com>
2026-09-08 13:04:21 -0600

    Update page.tsx

--- app/rsz-rentals/page.tsx
+++ app/rsz-rentals/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import SanMiguelContent from './rzrRentalContenido'
 
 export const metadata: Metadata = {
-  title: 'San Miguel de Allende Tours. ATV, RZR &  | Guey Tours',
+  title: 'San Miguel de Allende Tours: ATV, RZR & More | Guey Tours',
   description: 'Discover San Miguel de Allende tours with Guey Tours. Enjoy ATV, RZR, off-road and private adventures with local English-speaking guides. Book your experience here!',
   keywords: ['San Miguel de allende tours'],
 }
