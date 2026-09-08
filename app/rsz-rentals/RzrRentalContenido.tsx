411a986327dfc4c6a134eddf467b0a34f12006bb
herramientasseo4-sketch <herramientasseo4@gmail.com>
2026-09-08 13:23:02 -0600

    Update RzrRentalContenido.tsx

--- app/rsz-rentals/RzrRentalContenido.tsx
+++ app/rsz-rentals/RzrRentalContenido.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-export default function AtvRentalsContent() {
+export default function RzrRentalContenido() {
   const { lang } = useLang()
 
   const [form, setForm] = useState({ name: '', email: '', message: '' })
